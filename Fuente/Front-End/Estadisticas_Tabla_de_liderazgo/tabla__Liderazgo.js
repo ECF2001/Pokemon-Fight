fc06eb10fd989feaf6beb2f592380b93e71fe89d
Maria Angelica <mrodriguezco@ucenfotec.ac.cr>
2024-07-08 18:01:49 -0600

    Estadisticas
    Javascript para efectos de la presentacion del front-end

--- Fuente/Front-End/Estadisticas_Tabla_de_liderazgo/tabla__Liderazgo.js
+++ Fuente/Front-End/Estadisticas_Tabla_de_liderazgo/tabla__Liderazgo.js
@@ -1,10 +1,11 @@
-const jugadores = [
-    {nombre: 'Jugador1', puntos: 100, duelosGanados: 10, pokemon: 'Pikachu.png'},
-    {nombre: 'Jugador2', puntos: 90, duelosGanados: 9, pokemon: 'Charizard.png'},
-];
-
 document.addEventListener('DOMContentLoaded', () => {
+    const jugadores = [
+        {nombre: 'Jugador1', puntos: 100, duelosGanados: 10, pokemon: 'Pikachu.png'},
+        {nombre: 'Jugador2', puntos: 90, duelosGanados: 9, pokemon: 'Charizard.png'},
+    ];
+
     const tableBody = document.querySelector('#tabla_liderazgo tbody');
+    const formAgregarJugador = document.getElementById('formAgregarJugador');
 
     function actualizarTabla() {
         tableBody.innerHTML = '';
@@ -12,25 +13,23 @@ document.addEventListener('DOMContentLoaded', () => {
         jugadores.forEach(jugador => {
             const row = document.createElement('tr');
 
-            row.innerHTML = `
-            <tr>
+            row.innerHTML =  `
             <td>
-            <div class ="caja_Usuario">
-            <img class ="imagen_Personajes" src="${jugador.pokemon}">
-            ${jugador.nombre}
-            </div>
+                <div class="caja_Usuario">
+                    <img class="imagen_Personajes" src="${jugador.pokemon}">
+                    ${jugador.nombre}
+                </div>
             </td>
             <td>
-            <p>Puntos: ${jugador.puntos} </p>
-            <p>Duelos ganados: ${jugador.duelosGanados}</p>
+                <p>Puntos: ${jugador.puntos}</p>
+                <p>Duelos ganados: ${jugador.duelosGanados}</p>
             </td>
             <td>
-            <img class="imagen_PersonajesUtilizado" src="${jugador.pokemon}"
+                <img class="imagen_PersonajesUtilizado" src="${jugador.pokemon}">
             </td>
-            </tr>
-             `;
-             
-             tableBody.appendChild(row);
+        `;
+
+        tableBody.appendChild(row);
         });
     }
 
