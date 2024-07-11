a4eb6e126cb54229b9290ebd1c3d988f0f7d8d59
SamuelSolano15 <ssolano1505@gmail.com>
2024-07-11 13:55:16 -0600

    Create inicioSesion.js

---
+++ Fuente/Front-End/Inicio_Sesion/inicioSesion.js
@@ -0,0 +1,66 @@
+const formulario = document.getElementById("formulario")
+const $inputs = document.querySelector("#formulario input")
+
+const expresiones = {
+    correo: /^[a-zA-Z0-9\_]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/,
+     contraseña: /^[a-zA-Z0-9\_\-]{4,16}$/
+}
+
+const campos = {
+    correo: false, 
+    contraseña: false
+}
+
+const validarFormulario = (e) => {
+        switch (e.target.id){
+            case "correo":
+                validarCampo(expresiones.correo, e.target, "correo");
+                break; 
+            case "contraseña":
+                validarCampo(expresiones.contraseña, e.target, "contraseña");
+                break; 
+    }
+}
+
+const validarCampo =  (expresion, input, campo) => {
+     if (expresion.test(input.value)) {
+        document.getElementById(`grupo__${campo}`).classList.remove("formulario__general-incorrecto");// cambia la clase para asignar el color
+        document.getElementById(`grupo__${campo}`).classList.add("formulario__general-correcto");
+        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-exclamation-circle");
+        document.querySelector(`#grupo__${campo} i`).classList.add("fa-xmark");
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove("formulario__input-error-activo");
+        campos[campo] = true;
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add("formulario__general-incorrecto");
+        document.getElementById(`grupo__${campo}`).classList.remove("formulario__general-correcto");
+        document.querySelector(`#grupo__${campo} i`).classList.add("fa-exclamation-circle");
+        document.querySelector(`#grupo__${campo} i`).classList.remove("fa-xmark"); 
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add("formulario__input-error-activo");
+        campos[campo] = false;
+    }   
+}
+
+$inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+
+});
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    if (campos.correo && campos.contraseña) {
+
+
+        document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
+
+        setTimeout(() => {
+            document.querySelectorAll(".formulario__general--correcto").forEach((icono) => {
+                icono.classList.remove("formulario__general--correcto");
+            });
+
+           location.reload();
+        }, 4000);
+   
+    }
+});
