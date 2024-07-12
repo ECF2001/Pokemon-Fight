bd2b24c3336cb20c6730644283a838b777189203
Maria Angelica <mrodriguezco@ucenfotec.ac.cr>
2024-07-11 20:04:01 -0600

    EquipoPokemon JS
    cambios en el js

--- Fuente/Front-End/Equipo_Pokemon/EquipoPokemon.js
+++ Fuente/Front-End/Equipo_Pokemon/EquipoPokemon.js
@@ -1,1 +1,13 @@
-function equipoSeleccionado
+document.addEventListener('DOMContentLoaded', function () {
+    const filas = document.querySelectorAll('.decoracionTabla');
+
+    filas.forEach(function (fila) {
+        fila.addEventListener('click', function () {
+            filas.forEach(function (f) {
+                f.classList.remove('seleccionada');
+            });
+
+            fila.classList.add('seleccionada');
+        });
+    });
+});
