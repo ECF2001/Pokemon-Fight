c637c128fb63b28891d2737dae1b057eaf555867
andybayern <153257031+andybayern@users.noreply.github.com>
2024-07-14 22:23:48 -0600

    Cambios finales al carrusel
    Se cambia color de fondo a un contenedor para permitir contraste al selecicionar uno sobre otro

--- Fuente/Front-End/Elegir_Equipo/elegir__Equipo.js
+++ Fuente/Front-End/Elegir_Equipo/elegir__Equipo.js
@@ -3,19 +3,18 @@ let mostrar = 1;
 function carrousel() {
     const caja1 = document.querySelector('.caja_1');
     const caja2 = document.querySelector('.caja_2');
-    transicion = document.querySelector('caja_1.mostrar',
-        'caja_2.mostrar'); 
+    
 
     if (mostrar === 1) {
         caja1.style.display = 'block';
         caja2.style.display = 'none';
-        transicion = 'none';
+    
         
         mostrar = 0;
     } else {
         caja1.style.display = 'none';
         caja2.style.display = 'block';
-        transicion = 'none';
+        
         mostrar = 1;
     }
 }
