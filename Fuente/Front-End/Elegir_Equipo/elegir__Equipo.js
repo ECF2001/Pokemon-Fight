eaffa8d78fdf7b6d52ef148bb34d75cc74d1b87d
andybayern <153257031+andybayern@users.noreply.github.com>
2024-07-14 21:01:46 -0600

    cambios en carrousel
    cambios en carrousel sin imagenes

--- Fuente/Front-End/Elegir_Equipo/elegir__Equipo.js
+++ Fuente/Front-End/Elegir_Equipo/elegir__Equipo.js
@@ -1,23 +1,23 @@
-function loadPage(pageNumber) {
-    
-    console.log('Loading page' + pageNumber);
+let mostrar = 1;
+
+function carrousel() {
+    const caja1 = document.querySelector('.caja_1');
+    const caja2 = document.querySelector('.caja_2');
+    transicion = document.querySelector('caja_1.mostrar',
+        'caja_2.mostrar'); 
+
+    if (mostrar === 1) {
+        caja1.style.display = 'block';
+        caja2.style.display = 'none';
+        transicion = 'none';
+        
+        mostrar = 0;
+    } else {
+        caja1.style.display = 'none';
+        caja2.style.display = 'block';
+        transicion = 'none';
+        mostrar = 1;
+    }
 }
 
-document.addEventListener('DOMContentLoaded',function(){
 
-    const prevLink = document.getElementById('prev');
-    const nextLink = document.getElementById('next');
-    const currentPage = 1;
-
-    prevLink.addEventListener('click', function(event){
-        event.preventDefault();
-
-        console.log('Ir a la pagina anterior');
-    });
-
-    nextLink.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        console.log('Ir a la siguiente pagina');
-    });
-});
