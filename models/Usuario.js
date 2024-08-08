5e7d5a1dadeb40e774ae787b1355bb51d2b464fa
Maria Angelica <mrodriguezco@ucenfotec.ac.cr>
2024-08-07 19:34:03 -0600

    modelo usuario

---
+++ models/Usuario.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+
+let UsuarioSchema = new mongoose.Schema ({
+
+    identificacion: {type:Number, required:true, unique:true},
+    nombreUsuario:{type:String, required:true},
+    primerApellido:{type:String, required:true},
+    segundoApellido:{type:String, required:true},
+    correo:{type:String, required:true, unique:true},
+    contrasena: {type:String, required:true},
+    amigos:{type:String, required:true},
+    equipos:{type:String, required:true},
+    victorias:{type:Number, required:true},
+    derrotas:{type:Number, required:true},
+    fotoPerfil:{type:String, required:true}
+
+},{versionKey:false})
+
+let Usuario = new mongoose.model('Usuario', UsuarioSchema);
+
+module.exports = Usuario
