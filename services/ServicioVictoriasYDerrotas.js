6333eab3fe7d649e3c9d265bac10e88fe0062bf1
Maria Angelica <mrodriguezco@ucenfotec.ac.cr>
2024-08-08 22:29:53 -0600

    reportes

--- services/ServicioVictoriasYDerrotas.js
+++ services/ServicioVictoriasYDerrotas.js
@@ -1,3 +1,44 @@
+const Batalla = require('../models/Batalla');
 const Usuario = require('../models/Usuario');
-//victorias y derrotas por equipo
-const Equipo = require('../models/equipo');
+
+const obtenerVictoriasYDerrotas = async (nombreUsuario) => {
+    const usuario = await Usuario.findOne({nombreUsuario});
+    console.log(usuario);
+    const resultado = {
+        victorias: [],
+        derrotas:[]
+    };
+    if (!usuario) {
+        return (resultado);
+    }
+    const listaAmigos = usuario.amigos;
+    const batallas = await Batalla.find({
+        $or: [
+            {nombreUsuario1: usuario.nombreUsuario, nombreUsuario2: {$in: listaAmigos}},
+            {nombreUsuario2: usuario.nombreUsuario, nombreUsuario1: {$in: listaAmigos}},
+        ]
+    });
+    batallas.forEach(batalla => {
+        const datosBatalla = {
+            usuario1: {
+                foto: "",
+                nombreUsuario: batalla.nombreUsuario1,
+                equipo: batalla.nombreEquipo1
+            },
+            usuario2: {
+                foto: "",
+                nombreUsuario: batalla.nombreUsuario2,
+                equipo: batalla.nombreEquipo2
+            }
+        }
+        if (batalla.nombreUsuarioVencedor === nombreUsuario) {
+            resultado.victorias.push(datosBatalla);
+        } else {
+            resultado.derrotas.push(datosBatalla);
+        }
+    });
+    return resultado
+}
+module.exports = {
+    obtenerVictoriasYDerrotas
+}
