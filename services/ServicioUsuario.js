4ecbc68e4038aa1d77eec286b5405dc745843cfc
Maria Angelica <mrodriguezco@ucenfotec.ac.cr>
2024-08-14 19:24:57 -0600

    Update ServicioUsuario.js

--- services/ServicioUsuario.js
+++ services/ServicioUsuario.js
@@ -61,26 +61,6 @@ const cambiarContrasena = async (nombreUsuario, nuevaContrasena, confirmarContra
     }
 }
 
-    //Express session 
-    const idInicioSesion = async ( correo, contrasena, sessionID)=>{
-        try{
-            const usuario = await Usuario.findOne({ correo, contrasena});
-
-            if(!usuario){
-                return console.log('Correo o contrasena incorrectos')
-            }
-            
-
-        usuario.sessionId = sessionID 
-        await usuario.save();
-
-        return { error: false, usuarioId: usuario._id };
-        } catch (err) {
-        console.error(err);
-        return { error: true, status: 500, mensaje: 'Error en el servidor' };
-    }
-
-    }
 
 const obtenerFotos = async (listaNombreUsuario) => {
     const usuarios = await Usuario.find({
