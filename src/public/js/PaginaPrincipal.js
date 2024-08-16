d87e18eb2d9367019ea89ca3d65f902b5d69cec9
SamuelSolano15 <ssolano1505@gmail.com>
2024-08-15 20:12:45 -0600

    cambios

--- src/public/js/PaginaPrincipal.js
+++ src/public/js/PaginaPrincipal.js
@@ -47,3 +47,24 @@ function actualizarListaAmigos(amigos) {
 document.addEventListener('DOMContentLoaded', function () {
     obtenerAmigos();
 });
+
+// Ventana modal
+const  modal = document.getElementById("ventanaModal");
+// Botón que abre el modal
+const boton = document.getElementById("abrirModal");
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+var span = document.getElementsByClassName("cerrar")[0];
+// Cuando el usuario hace clic en el botón, se abre la ventana
+boton.addEventListener("click",function() {
+  modal.style.display = "block";
+});
+// Si el usuario hace clic en la x, la ventana se cierra
+span.addEventListener("click",function() {
+  modal.style.display = "none";
+});
+// Si el usuario hace clic fuera de la ventana, se cierra.
+window.addEventListener("click",function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
